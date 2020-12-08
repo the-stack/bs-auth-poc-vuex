6a1fc7b790415b8da9588bbbf76e38b136bb41f8
Rolf Eggenberger <rolf@kunstwort.com>
2020-12-08 16:37:05 +0100

    google config

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,16 +1,17 @@
-/*
+const base = 'https://modest-clarke-c9fc62.netlify.app/'
+
 export const oidcSettings = {
   authority: 'https://accounts.google.com',
   clientId: '500371192883-0i7pip8cetf33kn55sgiimg42qulbv35.apps.googleusercontent.com',
-  redirectUri: 'http://localhost:8080/callback',
-  popupRedirectUri: 'http://localhost:8080/oidc-popup-callback',
+  redirectUri: base + 'callback',
+  popupRedirectUri: base + 'oidc-popup-callback',
   responseType: 'id_token token',
   scope: 'openid profile',
   automaticSilentRenew: true,
   automaticSilentSignin: false,
-  silentRedirectUri: 'http://localhost:8080/silent-renew-oidc.html'
+  silentRedirectUri: base + 'silent-renew-oidc.html'
 }
-*/
+/*
 export const oidcSettings = {
   authority: 'https://pocid-id.bluesign.com',
   clientId: 'pocid',
@@ -22,3 +23,4 @@ export const oidcSettings = {
   automaticSilentSignin: false,
   silentRedirectUri: 'http://localhost:8080/silent-renew-oidc.html'
 }
+*/
